5d1a720a2e8c061e64958e9c8fde222c8534c165
nataliebrammerjensen <nabj@itu.dk>
2018-02-15 16:10:09 +0100

    First Commit

---
+++ app/src/main/java/com/example/nataliebrammerjensen/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.example.nataliebrammerjensen.geoquiz;
+
+/**
+ * Created by nataliebrammerjensen on 08/02/2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+
+
+
+        mAnswerTrue = answerTrue;
+    }
+}
